043c9ae5c0ab71c18843a318bac6f3621e9572b5
Franciel Cantanhede <franciel_cr@hotmail.com>
2022-10-31 21:17:42 -0300

    er2 uc15 - parte 1

---
+++ ER2/Projeto_teste/Projeto_teste/Projeto_teste/Operacoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_teste
+{
+    public static class Operacoes // static é para nao precisar instanciar para chamar
+    {
+        public static double Somar(double pNum, double sNum)
+        {
+            return (pNum + sNum);
+        }
+    }
+}
